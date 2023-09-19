a518f2b61384fa5b330e7b02a2c8dfbabbd01298
Juan Jose Gil <juan.j.gil.cortes@gmail.com>
2023-09-19 17:49:00 +0200

    findContour function added
    findContour added to contour.py file

--- contour.py
+++ contour.py
@@ -6,10 +6,12 @@ thresholdType,threshold = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
 
 print(f'Threshold type: {thresholdType}')
 
+contour, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(image, contour, -1, (255,63,52), 3)
 # Show image
 cv2.imshow('Original image', image)
-cv2.imshow('Processed image: Gray', gray)
-cv2.imshow('Processed image: Threshold', threshold)
+#cv2.imshow('Processed image: Gray', gray)
+#cv2.imshow('Processed image: Threshold', threshold)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
