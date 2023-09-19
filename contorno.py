69fa1066c0e2c9552b30cd24636f6d14b72cd9c8
Juan Jose Gil <juan.j.gil.cortes@gmail.com>
2023-09-19 16:42:43 +0200

    Initial Version

--- contorno.py
+++ contorno.py
@@ -2,7 +2,7 @@ import cv2
 
 image = cv2.imread('shape.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-thresholdType,threshold = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+thresholdType,threshold = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
 
 print(f'Threshold type: {thresholdType}')
 # Show image
